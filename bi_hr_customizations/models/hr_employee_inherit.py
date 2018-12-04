0bf2ed1416100b1e182e23d91ee0399880efc8e0
FatmaSharkawy <fdaify@iskyev.com>
2018-12-04 22:23:57 +0200

    [ADD] fields to employee form

--- bi_hr_customizations/models/hr_employee_inherit.py
+++ bi_hr_customizations/models/hr_employee_inherit.py
@@ -5,3 +5,21 @@ from odoo.exceptions import ValidationError
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
+
+    passport_expiry_date = fields.Date(string='Passport Expiry Date')
+    iqama_start_date = fields.Date(string='Iqama Start Date')
+    iqama_expiry_date = fields.Date(string='Iqama Expiry Date')
+    use_municipality_card = fields.Boolean(string='Use Municipality Card')
+    mc_start_date = fields.Date(string='MC Start Date')
+    mc_expiry_date = fields.Date(string='MC Expiry Date')
+
+
+    @api.multi
+    @api.constrains('iqama_start_date','iqama_expiry_date','mc_start_date','mc_expiry_date')
+    def check_dates(self):
+        for rec in self:
+            if rec.iqama_start_date > rec.iqama_expiry_date:
+                raise ValidationError(_('Iqama start date must be less than iqama expiry date.'))
+
+            if rec.mc_start_date > rec.mc_expiry_date:
+                raise ValidationError(_('MC start date must be less than mc expiry date.'))
