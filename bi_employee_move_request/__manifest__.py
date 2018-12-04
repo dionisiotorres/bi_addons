f262f1166e97c1ae401f232eb8ac43efcffe1e54
FatmaSharkawy <fdaify@iskyev.com>
2018-12-05 01:17:23 +0200

    [ADD] employee move request module

---
+++ bi_employee_move_request/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BI Employee Move Request",
+    'summary': "BI Employee Move Request",
+    'description': """ 
+        This module add fields to employee contract effect in payslip.
+     """,
+    'author': "BI Solutions Development Team",
+    'category': 'Human Resources',
+    'version': '0.1',
+    'depends': ['bi_employee_warehouse', 'hr_contract', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/employee_move_request_view.xml',
+        'views/menu_item_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'sequence': 1
+}
---
+++ bi_employee_move_request/models/employee_move_request.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
+
+
+class EmployeeMoveReason(models.Model):
+    _name = 'employee.move.reason'
+
+    name = fields.Char(string='Name', required=True)
+
+
+class EmployeeMoveRequest(models.Model):
+    _name = 'employee.move.request'
+    _inherit = ['mail.thread', 'resource.mixin']
+
+    name = fields.Char(string='Name', readonly=True)
+    request_date = fields.Date(string='Transfer Date', required=True)
+    employee_move_reason_id = fields.Many2one('employee.move.reason', string='Transfer Reason', required=True)
+    employee_id = fields.Many2one('hr.employee', string='Employee', required=True)
+    state = fields.Selection(
+        [('draft', 'Draft'), ('confirmed', 'Confirmed'),
+         ('cancelled', 'Cancelled')], string='State', default='draft', track_visibility='onchange')
+    employee_department_id = fields.Many2one('hr.department', string='Department', compute='get_employee_data',
+                                             store=True)
+    employee_analytic_account_id = fields.Many2one('account.analytic.account', string='Analytic Account',
+                                                   compute='get_employee_data',
+                                                   store=True)
+    employee_warehouse_id = fields.Many2one('stock.warehouse', string='Branch', compute='get_employee_data',
+                                            store=True)
+
+    department_id = fields.Many2one('hr.department', string='New Department', )
+    analytic_account_id = fields.Many2one('account.analytic.account', string='New Analytic Account', )
+    warehouse_id = fields.Many2one('stock.warehouse', string='New Branch')
+
+    @api.model
+    def create(self, vals):
+        vals['name'] = self.env['ir.sequence'].next_by_code('employee.move.request')
+        return super(EmployeeMoveRequest, self).create(vals)
+
+    @api.multi
+    def unlink(self):
+        for rec in self:
+            if rec.state == 'confirmed':
+                raise ValidationError(_('You cannot delete confirmed move request.'))
+        return super(EmployeeMoveRequest, self).unlink()
+
+    @api.multi
+    @api.depends('employee_id')
+    def get_employee_data(self):
+        for rec in self:
+            rec.employee_department_id = rec.employee_id.department_id.id
+            # rec.employee_analytic_account_id = rec.employee_id.contract_id.analytic_account_id.id
+            rec.employee_warehouse_id = rec.employee_id.warehouse_id.id
+
+    @api.multi
+    def set_state_to_confirmed(self):
+        for rec in self:
+            rec.state = 'confirmed'
+
+    @api.multi
+    def set_state_to_cancelled(self):
+        for rec in self:
+            rec.state = 'cancelled'
+
+    @api.multi
+    def set_state_to_draft(self):
+        for rec in self:
+            rec.state = 'draft'
