b98ed6cd8a1682b4b9f94f9d1c1d8cd6a9848fa9
FatmaSharkawy <fdaify@iskyev.com>
2019-01-01 03:19:08 +0200

    [ADD] default and domain for location in scrap order

--- bi_inventory_customization/models/stock_scrap_inherit.py
+++ bi_inventory_customization/models/stock_scrap_inherit.py
@@ -8,6 +8,21 @@ class StockScrap(models.Model):
     _order = 'id desc'
     _description = 'Scrap'
 
+    @api.model
+    def get_location_domain(self):
+        internal_location_objects = self.env['stock.location'].search([('usage', '=', 'internal')])
+        if self.env.user.warehouse_id:
+            return [('id', '=', self.env.user.warehouse_id.lot_stock_id.id)]
+        else:
+            return [('id', 'in', internal_location_objects.ids)]
+
+    def get_default_user_location_id(self):
+        warehouse = self.env.user.warehouse_id
+        if warehouse:
+            return warehouse.lot_stock_id.id
+        else:
+            return None
+
     product_uom_id = fields.Many2one('uom.uom', 'Unit of Measure', related='product_id.uom_id', required=True,
                                      readonly=True)
     state = fields.Selection([
@@ -16,6 +31,9 @@ class StockScrap(models.Model):
         ('done', 'Done')], string='Status', default="draft", track_visibility='onchange')
 
     reason = fields.Char(string='Reason', required=True)
+    location_id = fields.Many2one(
+        'stock.location', 'Location', domain=get_location_domain, default=get_default_user_location_id,
+        required=True, states={'done': [('readonly', True)]}, )
 
     @api.multi
     def action_approve(self):
