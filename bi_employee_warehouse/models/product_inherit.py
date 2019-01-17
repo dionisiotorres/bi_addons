b17d1495b157fdd599b213ceff21292dc666c3f9
Mohammed Ezzat <m.ezzat92@gmail.com>
2019-01-17 18:01:15 +0200

    [UPD] qty on hand location

--- bi_employee_warehouse/models/product_inherit.py
+++ bi_employee_warehouse/models/product_inherit.py
@@ -7,7 +7,7 @@ class ProductProductInherit(models.Model):
 
     def _get_domain_locations(self):
         res = super(ProductProductInherit,self)._get_domain_locations()
-        if self.env.context.get('with_user_warehouse'):
-            res[0].append(('location_id','child_of',self.env.user.dest_location_id.id))
+        if self.env.context.get('with_user_warehouse') and self.env.user.warehouse_id and self.env.user.warehouse_id.lot_stock_id:
+            res[0].append(('location_id','child_of',self.env.user.warehouse_id.lot_stock_id.id))
             return res
         return res
