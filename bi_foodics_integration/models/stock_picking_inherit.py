63340d58a4d4ebc840c9b14da33efed7d8e7882d
Mohammed Ezzat <m.ezzat92@gmail.com>
2019-04-10 21:49:47 +0200

    [ADD] bi_sale_stock_customization module

--- bi_foodics_integration/models/stock_picking_inherit.py
+++ bi_foodics_integration/models/stock_picking_inherit.py
@@ -14,8 +14,6 @@ class StockPickingInherit(models.Model):
         res = super(StockPickingInherit,self.sudo()).action_done()
         if self._context.get('keep_dates', False) and self._context.get('force_period_date', False):
             self.write({'date_done': self._context.get('force_period_date')})
-        else:
-            self.write({'date_done': fields.Datetime.now()})
         return res
 
     @api.multi
